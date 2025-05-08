a0e911ad2a4c3b1acd07dfd18913d0bf527fc8bf
hexahedron1 <elongatedsquareorthobicupola@gmail.com>
2025-05-08 19:56:53 +0300

    First commit

---
+++ HackerTerminal/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+if (!Directory.Exists($"/home/{Environment.UserName}/.local/share/hackerterminal"))
+    Directory.CreateDirectory($"/home/{Environment.UserName}/.local/share/hackerterminal");
